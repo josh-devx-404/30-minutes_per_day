a4347a85a257297e1feeea5caeeea22c79ec5c32
Josh <josh.ocampo200529@gmail.com>
2025-08-25 20:11:19 +0800

    Day 8: concatenate Boolean and string without getting an error

---
+++ Javascript/day7.js
@@ -0,0 +1,6 @@
+// Day 7: concatenate float and string without getting an error
+
+const floatNum = "True"
+const str = "Josh"
+
+console.log(`My name is ${str}, and I'm ${floatNum} years old.`);
---
+++ Javascript/day8.js
@@ -0,0 +1,6 @@
+// Day 8: concatenate Boolean and string without getting an error
+
+const str = "Josh"
+const Bool = true
+
+console.log(`My name is ${str}, and I'm not a minor: ${Bool}`);
